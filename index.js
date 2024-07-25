ae1a6dfde8b4ebf09e215adf2210493ff99f19bc
sbm-ocampagnoli <otavio.campagnoli@sbmtechnology.com.br>
2024-07-25 15:27:18 -0300

    Criando um token.

---
+++ index.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken');
+const secretKey = "skljaksdj9983498327453lsldkjf";
+
+const nossoToken = jwt.sign(
+    {
+      email: 'nome@alura.com.br',
+      password: 'HuEKW489!j445*',
+    },
+    secretKey,
+    {
+      expiresIn: '1y',
+      subject: '1',
+    }
+  );
+
+  console.log(nossoToken);
